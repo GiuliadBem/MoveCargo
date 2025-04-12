3c3fe6979cb958d58cd5264646b3d5c40bf8f1e2
GiuliadBem <giuliadbem@gmail.com>
2025-04-12 17:29:49 -0300

    Cria controlador caminhao

---
+++ controladores/controlador_caminhao.py
@@ -0,0 +1,3 @@
+class ControladorCaminhao:
+    def __init__(self):
+        pass
--- controladores/controlador_sistema.py
+++ controladores/controlador_sistema.py
@@ -1,6 +1,8 @@
+from controlador_caminhao import ControladorCaminhao
+
 class ControladorSistema:
     def __init__(self):
-        pass
+        self.__controlador_caminhao = ControladorCaminhao()
 
     def iniciar_sistema(self):
         print("Iniciando sistema...")
