8525f42098aef4434929333814e275eb15ff6f5f
GiuliadBem <giuliadbem@gmail.com>
2025-04-13 13:49:40 -0300

    Cria caminhao dao

---
+++ daos/caminhao_dao.py
@@ -0,0 +1,18 @@
+from dao import DAO
+from modelos.caminhao import Caminhao
+
+class CaminhaoDAO(DAO):
+    def __init__(self):
+        super().add("caminhao.pkl")
+
+    def add(self, caminhao: Caminhao):
+        super().add(caminhao.id, caminhao)
+
+    def update(self, caminhao: Caminhao):
+        super().update(caminhao.id, caminhao)
+
+    def get(self, chave: int):
+        return super().get(chave)
+    
+    def remove(self, chave: int):
+        return super().remove(chave)
